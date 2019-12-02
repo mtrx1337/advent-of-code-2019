b2b9614b7cc88147130df7c5aa60a87ba864d9c2
Leonard Lorenz <dev@leonardlorenz.de>
2019-12-02 18:45:54 +0100

    almost done with day2 part1

--- 02/part1/src/main.rs
+++ 02/part1/src/main.rs
@@ -28,15 +28,15 @@ fn main() {
 
     splits[1] = 12;
     splits[2] = 2;
-    for x in (0..99).step_by(4) {
+    for x in (0..splits.len()).step_by(4) {
         let a = splits[x + 1] as usize;
         let b = splits[x + 2] as usize;
         let c = splits[x + 3] as usize;
-        println!("{},{},{},{},", splits[x], a,b,c);
+        println!("{},{},{},{}", splits[x], a, b, c);
         match splits[x] {
             1 => splits[c] = splits[a] + splits[b],
             2 => splits[c] = splits[a] * splits[b],
-            99 => exit(0),
+            99 => break,
             _ => { println!("Invalid opcode."); exit(1) },
         }
     }
