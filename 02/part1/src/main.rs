7afbef94f79e2c9047421b4e11b150a0a81a6182
Leonard Lorenz <dev@leonardlorenz.de>
2019-12-02 12:09:04 +0100

    Almost done with day2 part1

---
+++ 02/part1/src/main.rs
@@ -0,0 +1,45 @@
+use std::string::String;
+use std::process::exit;
+use std::io::{self, Read};
+
+fn is_digits(this: &str) -> bool {
+    for x in this.chars() {
+        if ! x.is_digit(10) {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin()
+        .lock()
+        .read_to_string(&mut buffer)
+        .unwrap();
+
+    let mut splits : Vec<u32> = buffer
+        .split(',')
+        .filter(|x| is_digits(x))
+        .map(|x| x
+            .parse::<u32>()
+            .unwrap())
+        .collect();
+
+    splits[1] = 12;
+    splits[2] = 2;
+    for x in (0..99).step_by(4) {
+        let a = splits[x + 1] as usize;
+        let b = splits[x + 2] as usize;
+        let c = splits[x + 3] as usize;
+        println!("{},{},{},{},", splits[x], a,b,c);
+        match splits[x] {
+            1 => splits[c] = splits[a] + splits[b],
+            2 => splits[c] = splits[a] * splits[b],
+            99 => exit(0),
+            _ => { println!("Invalid opcode."); exit(1) },
+        }
+    }
+
+    println!("{}", splits[0]);
+}
